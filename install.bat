89e950442dbfbc6bff037a103c0025410d8fef68
Victor <vixstixboom@gmail.com>
2021-07-31 22:03:13 +0200

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,9 @@
+@echo off
+if exist primitive-cloud-server del /q primitive-cloud-server
+git clone https://github.com/SheepTester/primitive-cloud-server.git
+cd..
+cd nodejs2
+set 1=%cd%
+cd..
+cd server
+xcopy /i /s "%1%" "primitive-cloud-server"
