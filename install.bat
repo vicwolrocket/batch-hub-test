4b2bb70e8b2eb623b942b1d635e912c0f7ecd603
Victor <vixstixboom@gmail.com>
2021-08-01 10:42:08 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,9 +3,9 @@ if exist primitive-cloud-server del /q primitive-cloud-server
 git clone https://github.com/SheepTester/primitive-cloud-server.git
 cd..
 cd nodejs2
-set 1=%cd%
+set dir=%cd%
 cd..
 cd server
-xcopy /i /s "%1%" "primitive-cloud-server"
+xcopy /i /s "%dir%" "primitive-cloud-server"
 cd primitive-cloud-server
 npm install
